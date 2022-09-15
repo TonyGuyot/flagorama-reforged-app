1b28b1cf50976365d0966a452af3f0486b820b99
Tony Guyot <tguyot@yahoo.com>
2022-09-15 11:25:59 +0200

    Add license

--- app/src/main/kotlin/data/local/model/CountryOverviewEntity.kt
+++ app/src/main/kotlin/data/local/model/CountryOverviewEntity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Tony Guyot
+ * Copyright (C) 2022 Tony Guyot
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
